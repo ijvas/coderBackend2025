8811b7bdbea6ae8ccc0df40f9eed2c9173a225a9
Ismael Vasconcel <ijvasconcel@gmail.com>
2025-03-27 18:07:12 -0300

    corrección de la filmina6.js

--- Programacion Backend 1 - Desarrollo avanzado de backend/clase3/filmina6.js
+++ Programacion Backend 1 - Desarrollo avanzado de backend/clase3/filmina6.js
@@ -2,32 +2,40 @@ const generarNumeroAleatorio = (min,max) => Math.floor(Math.random() * (max - mi
 
 const generarNumerosAleatorios = () =>{
     return new Promise((resolve,reject) => {
+ 
         try {
-            
             const conteo = {}
-    
-            for (let i = 0; i = 10000; i++){
+
+            for (let i = 0; i < 10000; i++){
+ 
                 const num = generarNumeroAleatorio(1,20)
+
                 conteo[num] = (conteo[num] || 0) + 1
             }
-    
-            resolve(conteo)
 
+            resolve(conteo)
+ 
         } catch (error) {
             reject(error)
         }
     })
 }
+ 
+
+
+ 
 
 const ejecutarGeneracion = async() =>{
     try {
         const resultado = generarNumerosAleatorios()
+
         console.log('Proceso completado. Resultado: ')
+
         console.log(resultado)
+ 
     } catch (error) {
         console.log('Ocurrio un error: ', error)
     }
-
 }
 
 ejecutarGeneracion()
