69ec170a2e237b0f736d1063a5578b8f9d13cf3d
Ismael Vasconcel <ijvasconcel@gmail.com>
2025-07-23 20:27:28 -0300

    Creamos y mejoramos los endpoints de carts

--- Programacion Backend 1 - Desarrollo avanzado de backend/Entrega final/tercera_entrega/src/routes/cart.router.js
+++ Programacion Backend 1 - Desarrollo avanzado de backend/Entrega final/tercera_entrega/src/routes/cart.router.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import Cart from '../models/cart.model.js'
+import mongoose from 'mongoose'
 
 const cartsRouter = express.Router()
 
@@ -13,6 +14,15 @@ cartsRouter.post('/', async(req, res) => {
         res.status(500).json({ message: error.message});
     }
 });
+
+cartsRouter.get('/', async(req,res)=>{
+    try {
+        const carts = await Cart.find()
+        res.status(200).json({ status: "success", payload: carts })
+    } catch (error) {
+        res.status(404).json({ status: "error", message: error.message })
+    }
+})
 
 cartsRouter.get('/:cid', async(req, res) => {
     try {
@@ -32,11 +42,42 @@ cartsRouter.post('/:cid/product/:pid', async(req, res) => {
         const { quantity } = req.body
         
         const updatedCart = await Cart.findByIdAndUpdate(cid, { $push: { products: { product: pid, quantity} } }, { new: true })
+        if(!updatedCart) return res.status(404).json({ status: "error", message: "Carrito no encontrado"})
+
         res.status(201).json({ status: "success", payload: updatedCart })
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
+cartsRouter.put('/:cid', async(req,res)=>{
+    try {
+        const cid = req.params.cid
+        const cart = await Cart.findById(cid)
+        if(!cart) return res.status(404).json({ status: "error", message: "Carrito no encontrado" })
+
+        cart.products = []
+        await cart.save()
+
+        res.status(200).json({ status: "success", payload: cart })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+cartsRouter.delete('/:cid', async(req,res)=>{
+    try {
+        const cid = req.params.cid
+
+        const deletedCart = await Cart.findByIdAndDelete(cid)
+
+        if(!deletedCart) return res.status(404).json({ status: "error", message: "Carrito no encontrado" })
+
+        res.status(200).json({ status: "success", payload: deletedCart })
+
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "error al eliminar carrito" })
+    }
+})
 
 export default cartsRouter
