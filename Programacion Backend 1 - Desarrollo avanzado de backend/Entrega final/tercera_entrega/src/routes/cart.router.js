84647307548a2c51bc33c25b17b9bb7643042cd2
Ismael Vasconcel <ijvasconcel@gmail.com>
2025-07-24 14:56:28 -0300

    Agregamos los endpoints a cart.router que eliminan cierto producto de un carrito y el que permite modificar la cantidad de un elemento dentro de un carrito

--- Programacion Backend 1 - Desarrollo avanzado de backend/Entrega final/tercera_entrega/src/routes/cart.router.js
+++ Programacion Backend 1 - Desarrollo avanzado de backend/Entrega final/tercera_entrega/src/routes/cart.router.js
@@ -50,6 +50,25 @@ cartsRouter.post('/:cid/product/:pid', async(req, res) => {
     }
 });
 
+cartsRouter.delete('/:cid/product/:pid', async(req,res)=>{
+    try {
+        const { cid, pid } = req.params
+
+        const cart = await Cart.findById(cid)
+        if (!cart) return res.status(404).json({ status: "error", message: "Carrito no encontrado" })
+
+        cart.products = cart.products.filter(item => item.product.toString() !== pid)
+
+        await cart.save()
+
+        res.status(200).json({ status: "success", payload: cart })
+
+    } catch (error) {
+        res.status(500).json({ status: "error", message: error.message })
+    }
+})
+
+
 cartsRouter.put('/:cid', async(req,res)=>{
     try {
         const cid = req.params.cid
@@ -64,6 +83,26 @@ cartsRouter.put('/:cid', async(req,res)=>{
         res.status(500).json({ message: error.message })
     }
 })
+
+
+cartsRouter.put('/:cid/product/:pid', async(req,res)=>{
+    try {
+        const { cid, pid } = req.params
+        const { quantity } = req.body
+        const { ObjectId } = mongoose.Types
+
+        if (!quantity || quantity < 1) return res.status(400).json({ status: "error", message: "La cantidad debe ser mayor o igual a 1" })
+        
+        const updatedCart = await Cart.findByIdAndUpdate(cid, { $set: { "products.$[elem].quantity": quantity } }, { new: true, arrayFilters: [{ "elem.product": new ObjectId(pid) }] })
+        if(!updatedCart) return res.status(404).json({ status: "error", message: "Carrito no encontrado"})
+
+        res.status(200).json({ status: "success", payload: updatedCart })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
 
 cartsRouter.delete('/:cid', async(req,res)=>{
     try {
